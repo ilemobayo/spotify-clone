1b6187d480affe95ca118b5d33898d116771552b
marc9323 <marcholman9323@gmail.com>
2018-01-16 19:01:40 -0600

    added shuffle button

--- index.php
+++ index.php
@@ -16,8 +16,39 @@ include("includes/config.php");
 <html>
     <head>
         <title>Welcome to Slotify!</title>
+        <link rel="stylesheet" type="text/css" href="assets/css/style.css" />
     </head>
     <body>
-    INDEX PAGE
+    
+        <div id="nowPlayingBarContainer">
+
+            <div id="nowPlayingBar">
+
+                <div id="nowPlayingLeft">
+
+                </div>
+
+                <div id="nowPlayingCenter">
+
+                    <div class="content playerControls">
+
+                        <div class="buttons">
+                            <button class="controlButton shuffle" title="Shuffle button">
+                                <img src="assets/images/icons/shuffle.png" alt="shuffle button" />>
+                            </button>
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div id="nowPlayingRight">
+
+                </div>
+
+            </div>
+
+        </div>
+
     </body>
 </html>
